02b813a60738a6c26d63ef354eb60b1cd04d3f8d
Lars Ole Simonsen <los@sirenia.eu>
2023-09-15 16:25:24 +0200

    - Get rid of jvm version warning - Get tracker running

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,7 @@ pluginManagement {
 
 rootProject.name = "shoot2"
 
-include(":ipv8")
+include(":ipv8", ":tracker", ":ipv8-jvm")
 project(":ipv8").projectDir = file("kotlin-ipv8/ipv8")
+project(":tracker").projectDir = file("kotlin-ipv8/tracker")
+project(":ipv8-jvm").projectDir = file("kotlin-ipv8/ipv8-jvm")
