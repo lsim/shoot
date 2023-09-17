ddfcf3654806443c996223b182e663980874b467
Lars Ole Simonsen <los@sirenia.eu>
2023-09-17 18:24:08 +0200

    message id should only be one byte

--- src/main/kotlin/comms/messages/GreetingMessage.kt
+++ src/main/kotlin/comms/messages/GreetingMessage.kt
@@ -6,7 +6,7 @@ import nl.tudelft.ipv8.messaging.Serializable
 data class GreetingMessage(val userName: String) : Serializable {
     override fun serialize(): ByteArray = userName.toByteArray()
     companion object Deserializer : Deserializable<GreetingMessage> {
-        const val MESSAGE_ID = 1337_1
+        const val MESSAGE_ID = 130
         override fun deserialize(buffer: ByteArray, offset: Int): Pair<GreetingMessage, Int> {
             return Pair(GreetingMessage(String(buffer)), buffer.size)
         }
